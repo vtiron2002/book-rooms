760033dc9d421132a85458c746527c9af0265a9c
vtiron2002 <vtiron2002@gmail.com>
2020-05-05 22:02:03 -0400

    Updates

---
+++ precache-manifest.feff8c8075f003b45e832d6a5af57ab5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba1ed2cd77517254d391c29eaacced0",
+    "url": "/book-rooms/index.html"
+  },
+  {
+    "revision": "610991c72b79c423dcdf",
+    "url": "/book-rooms/static/css/main.28b702f1.chunk.css"
+  },
+  {
+    "revision": "82e57ad9e4edbeeaf053",
+    "url": "/book-rooms/static/js/2.c5b08ece.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book-rooms/static/js/2.c5b08ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610991c72b79c423dcdf",
+    "url": "/book-rooms/static/js/main.749b8409.chunk.js"
+  },
+  {
+    "revision": "0fafa3b61ab8f07c5263",
+    "url": "/book-rooms/static/js/runtime-main.2ff0a064.js"
+  }
+]);
